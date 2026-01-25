9cf6a2b102fda32aac9a5a25bd51609a2887a677
itsnexverra <itsnexverra@gmail.com>
2026-01-25 18:26:33 +0530

    Add files via upload

---
+++ backend/constant.tsx
@@ -0,0 +1,17 @@
+export const products = [
+  {
+    "id": "1948af85-31ad-43d3-91a6-e80969bb7beb",
+    "title": "Odoo Dashboard",
+    "description": "sfsdf",
+    "features": [
+      "chat"
+    ],
+    "images": [
+      "https://live.staticflickr.com/65535/55027405650_e80bdd31d5_k.jpg"
+    ],
+    "price": 230000,
+    "category": "business",
+    "type": "dashboard",
+    "downloadableFileName": null
+  }
+];
