4539e39e7e13e76cc5550d52b3caad0944e19955
itsnexverra <itsnexverra@gmail.com>
2026-01-25 18:51:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -450,7 +450,7 @@ app.get('/api/admin/orders', authenticateToken, authenticateAdmin, async (req, r
 // SPA Serving
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Startup
